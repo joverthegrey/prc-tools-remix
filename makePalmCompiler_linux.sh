11ade73c4b890cf8e3a5d8f69cd297e700ea99af
meepingsnesroms <guicrith@gmail.com>
2018-03-29 18:42:55 -0700

    Add gitignore, fix 1 click makefiles

--- makePalmCompiler_linux.sh
+++ makePalmCompiler_linux.sh
@@ -7,5 +7,5 @@ echo "Building from '$DIR'"
 rm -rf $DIR/output
 mkdir $DIR/output
 cd $DIR/output
-../prc-tools-remix/prc-tools-2.3/configure --enable-languages=c,c++ --enable-targets=m68k-palmos,arm-palmos --with-palmdev-prefix=../palmSdk/sdk-5r4 --host=i686-linux-gnu
+../prc-tools-2.3/configure --enable-languages=c,c++ --enable-targets=m68k-palmos,arm-palmos --disable-nls --with-palmdev-prefix=../palmSdk/sdk-5r4 --host=i686-linux-gnu
 make
