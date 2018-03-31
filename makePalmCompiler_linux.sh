af024ad1e6d20ef46382ab2bff6682cf38548382
meepingsnesroms <guicrith@gmail.com>
2018-03-31 13:18:04 -0700

    Fix missing sdk errors

--- makePalmCompiler_linux.sh
+++ makePalmCompiler_linux.sh
@@ -7,7 +7,7 @@ echo "Building from '$DIR'"
 rm -rf $DIR/output
 mkdir $DIR/output
 cd $DIR/output
-../prc-tools-2.3/configure --enable-languages=c,c++ --enable-targets=m68k-palmos,arm-palmos --disable-nls --with-palmdev-prefix=../palmSdk --host=i686-linux-gnu
+../prc-tools-2.3/configure --enable-languages=c,c++ --enable-targets=m68k-palmos,arm-palmos --disable-nls --with-palmdev-prefix=$DIR/palmSdk --host=i686-linux-gnu
 make
 sudo make install
 sudo palmdev-prep
