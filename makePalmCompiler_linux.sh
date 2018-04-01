f631da44e4d4ebe3556ed123921cb21d23d5343c
meepingsnesroms <guicrith@gmail.com>
2018-04-01 13:28:21 -0700

    More fixes

--- makePalmCompiler_linux.sh
+++ makePalmCompiler_linux.sh
@@ -8,7 +8,7 @@ echo "Building from '$DIR'"
 rm -rf $DIR/output
 mkdir $DIR/output
 cd $DIR/output
-../prc-tools-2.3/configure --enable-languages=c,c++ --enable-targets=m68k-palmos,arm-palmos --disable-nls --with-palmdev-prefix=$DIR/palmSdk --host=i686-linux-gnu
+../prc-tools-2.3/configure --enable-languages=c,c++ --enable-targets=m68k-palmos,arm-palmos --disable-nls --disable-doc --with-palmdev-prefix=$DIR/palmSdk --host=i686-linux-gnu
 make
 sudo make install
 sudo palmdev-prep
