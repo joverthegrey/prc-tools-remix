bc00733f49debda0328f1bc700c5e78ebbf69918
meepingsnesroms <guicrith@gmail.com>
2019-04-07 11:53:42 -0700

    Update makePalmCompiler_macOS.sh

--- makePalmCompiler_macOS.sh
+++ makePalmCompiler_macOS.sh
@@ -6,31 +6,31 @@ echo "Building from '$DIR'"
 
 # prc-tools
 rm -rf $DIR/output
-mkdir $DIR/output
+mkdir -p $DIR/output || exit
 cd $DIR/output
-../prc-tools-2.3/configure --enable-languages=c,c++ --enable-targets=m68k-palmos,arm-palmos --disable-nls --disable-doc --with-palmdev-prefix=$DIR/palmSdk --host=i686-apple-darwin
-make
-sudo make install
-sudo palmdev-prep
-sudo palmdev-prep --default 3.5
+../prc-tools-2.3/configure --enable-languages=c,c++ --enable-targets=m68k-palmos,arm-palmos --disable-nls --disable-doc --with-palmdev-prefix=$DIR/palmSdk --host=i686-apple-darwin || exit
+make || exit
+sudo make install || exit
+sudo palmdev-prep || exit
+sudo palmdev-prep --default 3.5 || exit
 
 # pilrc
 rm -rf $DIR/pilrc_out
-mkdir $DIR/pilrc_out
+mkdir -p $DIR/pilrc_out || exit
 cd $DIR/pilrc_out
-../pilrc/unix/configure
-make
-sudo cp ./pilrc /usr/local/bin
+../pilrc/unix/configure || exit
+make || exit
+sudo cp ./pilrc /usr/local/bin || exit
 
 # pilot-xfer
 rm -rf $DIR/pilot-link_out
-mkdir $DIR/pilot-link_out
+mkdir -p $DIR/pilot-link_out || exit
 cd $DIR/pilot-link_out
-../pilot-link-0.12.0-pre2/configure --with-libpng
-make
-sudo cp ./src/pilot-xfer /usr/local/bin
-sudo mkdir -p /usr/local/bin/.libs
-sudo cp -a ./src/.libs/. /usr/local/bin/.libs
-sudo cp ./libpisock/.libs/libpisock.9.0.0.dylib /usr/local/lib
-sudo cp ./libpisock/.libs/libpisock.9.dylib /usr/local/lib
-sudo cp ./libpisock/.libs/libpisock.dylib /usr/local/lib
+../pilot-link-0.12.0-pre2/configure --with-libpng || exit
+make || exit
+sudo cp ./src/pilot-xfer /usr/local/bin || exit
+sudo mkdir -p /usr/local/bin/.libs || exit
+sudo cp -a ./src/.libs/. /usr/local/bin/.libs || exit
+sudo cp ./libpisock/.libs/libpisock.9.0.0.dylib /usr/local/lib || exit
+sudo cp ./libpisock/.libs/libpisock.9.dylib /usr/local/lib || exit
+sudo cp ./libpisock/.libs/libpisock.dylib /usr/local/lib || exit
